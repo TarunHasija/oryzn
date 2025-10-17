a2d929af2b510adb2c66837ae90cabbf855c3b92
TarunHasija <tarunHasija999@gmail.com>
2025-10-18 04:11:03 +0530

    refactor: update Home widget to use ConsumerWidget and improve text display

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final hello = Provider<String>((ref){
+final hello = Provider<String>((ref) {
   return 'Hello, Riverpod!';
 });
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final text = ref.watch(hello);
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: const Center(child: Text('Welcome to the Home Page!')),
+      body: Center(child: Text(text)),
     );
   }
 }
