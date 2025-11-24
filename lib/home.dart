91bc05e9ac184d827b63a3b3e554bac18c75f066
TarunHasija <tarunHasija999@gmail.com>
2025-11-24 17:47:47 +0530

    ..

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final hello = Provider<String>((ref) {
-  return 'Hello, Riverpod!';
-});
+import 'package:oryzn/provider/counter_notifier.dart';
 
 class Home extends ConsumerWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final text = ref.watch(hello);
+    print("Build");
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: Center(child: Text(text)),
+      appBar: AppBar(title: Text("Hello World!")),
+      body: Column(
+        children: [
+          Consumer(
+            builder: (context, ref, child) {
+              final count = ref.watch(counterNotifier);
+              return Text(count.toString());
+            },
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(counterNotifier.notifier).increment();
+                },
+                child: Text("+"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(counterNotifier.notifier).decrement();
+                },
+                child: Text("-"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(counterNotifier.notifier).reset();
+                },
+                child: Text("Reset"),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oryzn/home.dart';
+import 'package:oryzn/provider/theme_provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
-
-class MyApp extends StatelessWidget {
-  const MyApp({ super.key });
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ProviderScope(
-      
-      child: MaterialApp(
-        home: Home(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDark = ref.watch(themeProvider);
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: isDark ? Brightness.dark : Brightness.light,
+        colorSchemeSeed: isDark ? Colors.blue : Colors.amber,
       ),
+
+      home: Home(),
     );
   }
 }
